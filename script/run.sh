27a36d8f649e57e2d88e1d8b3f87be084eebf418
Cheng Jian <gatieme@163.com>
2018-11-17 17:35:25 +0800

    change relative_path to abbsolute_path

--- script/run.sh
+++ script/run.sh
@@ -58,7 +58,7 @@ qemu_run_kernel( )
 		echo "image $KERNEL_IMAGE not found"
 		echo "you should build your kernel first"
 		echo "run the next :"
-		echo "\t$common build $ARCH $BUILD_ROOT_DIR"
+		echo "\t$common build $ARCH $BUILD_ROOT_SCRIPT_DIR"
 		exit 1
 	fi
 
@@ -146,7 +146,7 @@ running_linux_kernel( )
 {
 	local TO_DO=$1
 	local ARCH=$2
-	local BUILD_ROOT_DIR=$3
+	local BUILD_ROOT_SCRIPT_DIR=$3
 
 	if [ $TO_DO = "build" ]; then
 		qemu_build_kernel
@@ -168,26 +168,27 @@ fi
 
 TO_DO=$1
 ARCH=$2
-BUILD_ROOT_DIR=`get_fullpath $3`
-BUILD_OUTPUT_DIR=$BUILD_ROOT_DIR/build/$ARCH
-BUILD_KERNEL_DIR=$BUILD_ROOT_DIR/$KERNEL_NAME
-BUILD_PATCH_DIR=#BUILD_ROOT_DIR/patch
+BUILD_ROOT_SCRIPT_DIR=$(cd $(dirname $3); pwd)
+BUILD_OUTPUT_DIR=$BUILD_ROOT_SCRIPT_DIR/build/$ARCH
+BUILD_KERNEL_DIR=$BUILD_ROOT_SCRIPT_DIR/$KERNEL_NAME
+BUILD_PATCH_DIR=#BUILD_ROOT_SCRIPT_DIR/patch
 
 VMLINUX_FILE=$BUILD_OUTPUT_DIR/vmlinux
 CONFIG_FILE=$BUILD_OUTPUT_DIR/.config
 
 QEMU_SYSTEM_DIR=/opt/software/toolchain/qemu/bin
 
-ROOT_DIR=..
-INITRDFS=$ROOT_DIR/filesystem/initrdfs/$ARCH/rootfs.cpio.gz
-VIRFS=$ROOT_DIR/filesystem/9p_virfs/$ARCH
+CURR_SCRIPT_PATH=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
+ROOT_SCRIPT_DIR=$CURR_SCRIPT_PATH/..
+INITRDFS=$ROOT_SCRIPT_DIR/filesystem/initrdfs/$ARCH/rootfs.cpio.gz
+VIRFS=$ROOT_SCRIPT_DIR/filesystem/9p_virfs/$ARCH
 
 
-BAKCUP_DIR=$ROOT_DIR/backup/$ARCH
+BAKCUP_DIR=$ROOT_SCRIPT_DIR/backup/$ARCH
 
 
-if [ ! -d "$BUILD_ROOT_DIR" ];then
-	echo "ERROR $BUILD_ROOT_DIR Not found"
+if [ ! -d "$BUILD_ROOT_SCRIPT_DIR" ];then
+	echo "ERROR $BUILD_ROOT_SCRIPT_DIR Not found"
 	echo "It's the place where your kernel_src, build, patch stay"
 	echo "you should mkdir $KERNEL_NAME, build in it"
 	echo
@@ -225,4 +226,4 @@ case $ARCH in
 esac
 
 
-running_linux_kernel $TO_DO $ARCH $BUILD_ROOT_DIR
+running_linux_kernel $TO_DO $ARCH $BUILD_ROOT_SCRIPT_DIR
